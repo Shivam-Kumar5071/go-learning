6ae7b30cdf7f1a918605e1d4c01ab2577c66772e
Shivam Kumar <kumar.s@razorpay.com>
2025-08-26 17:59:20 +0530

    learning some more concepts in file handling

--- Files/files.go
+++ Files/files.go
@@ -25,6 +25,38 @@ func main(){
 	fmt.Println(fileInfo.Mode())
 	fmt.Println(fileInfo.Size())
 
+	
+	// buff := make([]byte,fileInfo.Size())
+	// d,err := f.Read(buff)
+	// if err != nil{
+	// 	panic(err)
+	// }
+
+	// for i:=0 ; i<len(buff);i++{
+	// 	fmt.Println("data : ",d,string(buff[i]))
+	// }
+
+	buf := make([]byte,fileInfo.Size())
+	c,err := f.Read(buf)
+	if err != nil {
+		panic(err)
+	}
+
+	for i:=0;i<len(buf);i++{
+		fmt.Println("data ",c,string(buf[i]))
+	}
+
+
+	//another way to read file and efficient way.
+	data , err := os.ReadFile("example.txt")
+	
+	if err != nil{
+		panic(err)
+	}
+	fmt.Println(string(data))
+
+	defer f.Close()
+
 
 
 }
