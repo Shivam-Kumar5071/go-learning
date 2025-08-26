86791b5e680d1be7b4f45853a94f7972ffc32355
Shivam Kumar <kumar.s@razorpay.com>
2025-08-26 16:15:36 +0530

    adding file system basic

--- Files/files.go
+++ Files/files.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main(){
-	fmt.Println("File System")
+	
+	f,err := os.Open("example.txt")
+
+	if err != nil{
+		panic(err)
+	}
+
+	fileInfo , err := f.Stat()
+
+	if err != nil{
+		//this means that error is not nil , or error encountered then you have to log the error.
+		panic(err)
+	}
+
+	fmt.Println(fileInfo.IsDir())
+	fmt.Println(fileInfo.Name())
+	fmt.Println(fileInfo.Mode())
+	fmt.Println(fileInfo.Size())
+
+
+
 }
