bd250a2b563a84355dd5c20beecb0d96ab54cac7
Shivam Kumar <kumar.s@razorpay.com>
2025-08-28 11:21:41 +0530

    adding some comments

--- Files/files.go
+++ Files/files.go
@@ -10,10 +10,11 @@ func main(){
 	f,err := os.Open("example.txt")
 
 	if err != nil{
+		//logging the error.
 		panic(err)
 	}
 
-	fileInfo , err := f.Stat()
+	fileInfo , err := f.Stat()//this will all the statical data about file.
 
 	if err != nil{
 		//this means that error is not nil , or error encountered then you have to log the error.
@@ -36,7 +37,7 @@ func main(){
 	// 	fmt.Println("data : ",d,string(buff[i]))
 	// }
 
-	buf := make([]byte,fileInfo.Size())
+	buf := make([]byte,fileInfo.Size()) //making of a buffer which is of array of bytes which stores information about the file i.e what is written like this.
 	c,err := f.Read(buf)
 	if err != nil {
 		panic(err)
@@ -47,7 +48,7 @@ func main(){
 	}
 
 
-	//another way to read file and efficient way.
+	//another way to read file and shortest way.
 	data , err := os.ReadFile("example.txt")
 	
 	if err != nil{
